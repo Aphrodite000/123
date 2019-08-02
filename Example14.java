995382b03f3b7691d673a56b0802d5357f3d3cea
aphrodite000 <153118976@qq.com>
2019-08-02 20:23:11 +0800

    计算圆周率

---
+++ Example14.java
@@ -0,0 +1,16 @@
+//求圆周率近似值
+public class Example14{
+	public static void main(String[] args) {
+		int i=1,sign=1;
+		double item,pi=0;
+		
+		do{
+			item=sign*1.0/i;
+			pi=pi+item;
+			i+=2;
+			sign=-sign;
+		}while(Math.abs(item)>1.0E-6);
+		pi=4*pi;
+		System.out.printf("PI=%.6f\n",pi);
+	}
+}
